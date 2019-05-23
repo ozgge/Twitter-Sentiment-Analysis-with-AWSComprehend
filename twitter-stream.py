4bbad9122bb7089356c0eb9ce7894e1daa24f56c
Ozge <44693142+ozgge@users.noreply.github.com>
2019-05-23 14:13:54 +0300

    Add files via upload

---
+++ twitter-stream.py
@@ -0,0 +1,45 @@
+import boto3
+import random
+import time
+import json
+
+from tweepy.streaming import StreamListener
+from tweepy import OAuthHandler
+from tweepy import Stream
+from configparser import ConfigParser
+
+parser = ConfigParser()
+
+parser.read('api_auth.cfg')
+access_token = parser.get('api_tracker', 'access_token')
+access_token_secret = parser.get('api_tracker', 'access_token_secret')
+consumer_key = parser.get('api_tracker', 'consumer_key')
+consumer_secret = parser.get('api_tracker', 'consumer_secret')
+aws_key_id =  parser.get('api_tracker', 'aws_key_id')
+aws_key =  parser.get('api_tracker', 'aws_key')
+
+DeliveryStreamName = 'twitter-stream'
+client = boto3.client('firehose', region_name='us-west-2',
+                          aws_access_key_id=aws_key_id,
+                          aws_secret_access_key=aws_key
+                          )
+class StdOutListener(StreamListener):
+
+    def on_data(self, data):
+
+        client.put_record(DeliveryStreamName=DeliveryStreamName,Record={'Data': json.loads(data)["text"]})
+
+        print (json.loads(data)["text"])
+
+        return True
+
+    def on_error(self, status):
+        print(status)
+
+    if __name__ == '__main__':
+        # This handles Twitter authetification and the connection to Twitter Streaming API
+        l = StdOutListener()
+        auth = OAuthHandler(consumer_key, consumer_secret)
+        auth.set_access_token(access_token, access_token_secret)
+        stream = Stream(auth, l)
+        stream.filter(track=['gameofthrones'])
